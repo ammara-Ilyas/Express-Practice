12989c0f3b84cf44ba9fd30ccb81113f3e8fd3d9
ammara-Ilyas <ismarailyas545@gmail.com>
2024-07-14 18:50:46 +0500

    partial parts are added

--- dynamic_express/src/index.js
+++ dynamic_express/src/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const path = require("path");
+const hbs = require("hbs");
 const app = express();
-
-app.set("views", path.join(__dirname, "../views"));
+////can change view name
+const templatePath = path.join(__dirname, "../templates/views");
+app.set("views", templatePath);
+const partialPath = path.join(__dirname, "../templates/partials");
 app.set("view engine", "hbs");
+hbs.registerPartials(partialPath);
 
 app.get("/", (req, res) => {
   res.render("index", {
